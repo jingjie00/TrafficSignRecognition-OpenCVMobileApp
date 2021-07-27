368b044001b895f795f3ea740d9a7528ede19dea
JJTAN0611 <2000tanjingjie@gmail.com>
2021-07-28 01:14:59 +0800

    Done basic

--- app/src/main/java/com/example/opencvproject/SplashScreen.java
+++ app/src/main/java/com/example/opencvproject/SplashScreen.java
@@ -2,14 +2,20 @@ package com.example.opencvproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 
 public class SplashScreen extends AppCompatActivity {
 
+    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +23,27 @@ public class SplashScreen extends AppCompatActivity {
         getSupportActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent =new Intent(SplashScreen.this, MainActivity.class);
-                finish();
-                startActivity(intent);
-            }
-        },2000);
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions
+                .request(Manifest.permission.CAMERA)
+                .subscribe(granted -> {
+                    if (granted) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent =new Intent(SplashScreen.this, MainActivity.class);
+                                finish();
+                                startActivity(intent);
+                            }
+                        },2000);
+                        // All requested permissions are granted
+                    } else {
+                        Toast.makeText(this, "Permissions Denied. Please Accept the Needed Permissions. ", Toast.LENGTH_LONG).show();
+                        // At least one permission is denied
+                    }
+                });
+
+
 
     }
 }
